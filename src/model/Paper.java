424bc329ebc57629dae6ba16eacdbfe38910e90e
Mert Türker <mertturker99@gmail.com>
2023-05-16 23:25:28 +0300

    Update Paper.java

--- src/model/Paper.java
+++ src/model/Paper.java
@@ -2,25 +2,27 @@ package model;
 
 // Paper.java
 public abstract class Paper {
-    private String[] authors;
+    private String authors;
     private String title;
-    private int year;
+    private String year;
     private String doi;
-
-    public Paper(String[] authors, String title, int year, String doi) {
+    private int downloadNumber;
+    
+    public Paper(String authors, String title, String year, String doi, int downloadNumber) {
         this.authors = authors;
         this.title = title;
         this.year = year;
         this.doi = doi;
+        this.downloadNumber = downloadNumber;
     }
 
     // Getters and setters
 
-    public String[] getAuthors() {
+    public String getAuthors() {
         return authors;
     }
 
-    public void setAuthors(String[] authors) {
+    public void setAuthors(String authors) {
         this.authors = authors;
     }
 
@@ -32,11 +34,11 @@ public abstract class Paper {
         this.title = title;
     }
 
-    public int getYear() {
+    public String getYear() {
         return year;
     }
 
-    public void setYear(int year) {
+    public void setYear(String year) {
         this.year = year;
     }
 
@@ -47,4 +49,12 @@ public abstract class Paper {
     public void setDoi(String doi) {
         this.doi = doi;
     }
+
+	public int getDownloadNumber() {
+		return downloadNumber;
+	}
+
+	public void setDownloadNumber(int downloadNumber) {
+		this.downloadNumber = downloadNumber;
+	}
 }

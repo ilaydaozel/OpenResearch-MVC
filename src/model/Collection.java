d62f0f80da6b9ec9bd4b5e5586028080eada8ac1
Mert Türker <mertturker99@gmail.com>
2023-05-19 18:33:47 +0300

    Create Collection.java

---
+++ src/model/Collection.java
@@ -0,0 +1,45 @@
+package model;
+
+import java.util.List;
+import java.util.Map;
+import java.util.ArrayList;
+import interfaces.IFileReader;
+
+public abstract class Collection {
+    private List<Object> collection = new ArrayList<>();
+    private IFileReader reader;
+
+    public Collection() {
+        
+        
+    }
+
+    public abstract void createCollection();
+
+    public abstract Object createCollectionElement(Map<String, String> data);
+
+    public List<Object> getCollection() {
+        return collection;
+    }
+
+    public void setCollection(List<Object> collection) {
+        this.collection = collection;
+    }
+
+
+    public void addToCollection(Object item) {
+    	collection.add(item);
+    }
+
+    public void removeFromItems(Object item) {
+    	collection.remove(item);
+    }
+
+	public IFileReader getReader() {
+		return reader;
+	}
+
+	public void setReader(IFileReader reader) {
+		this.reader = reader;
+	}
+}

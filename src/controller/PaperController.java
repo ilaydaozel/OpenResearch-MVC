0511340d31af408aaad15af38afa8cad20cd7cfa
Mert Türker <mertturker99@gmail.com>
2023-05-19 20:57:00 +0300

    Update PaperController.java

--- src/controller/PaperController.java
+++ src/controller/PaperController.java
@@ -2,6 +2,13 @@ package controller;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import javax.swing.JOptionPane;
+
 import model.*;
 import view.*;
 
@@ -11,7 +18,8 @@ public class PaperController {
 	
 	public PaperController(PapersPage papersView) {
 		this.papersView = papersView;
-		papersView.selectPaper(new SelectPaperListener());;
+		papersView.selectPaper(new SelectPaperListener());
+		papersView.downloadFile(new DownloadPaperListener());
 	}
 
 	class SelectPaperListener implements ActionListener{
@@ -24,5 +32,18 @@ public class PaperController {
 
 		}
 	}
-
+	class DownloadPaperListener implements ActionListener {
+	    public void actionPerformed(ActionEvent e) {
+	        try {
+	            Path source = Paths.get("OpenResearch-MVC/src/data/A_2005_Introducing Test Automation and Test-Driven Development.pdf");
+	            Path destination = Paths.get("downloaded_file.pdf");
+	
+	            Files.copy(source, destination);
+	            JOptionPane.showMessageDialog(papersView, "File downloaded successfully!");
+	        } catch (IOException ex) {
+	            ex.printStackTrace();
+	            JOptionPane.showMessageDialog(papersView, "Error downloading file: " + ex.getMessage());
+	        }
+	    }
+}
 }

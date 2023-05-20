86aac127c96b2a10c3ce8dcf6d07fa6355e6e04f
Mert Türker <mertturker99@gmail.com>
2023-05-20 13:12:44 +0300

    Update XmlFileIO.java

--- src/fileIO/XmlFileIO.java
+++ src/fileIO/XmlFileIO.java
@@ -1,13 +1,23 @@
 package fileIO;
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.StringWriter;
+import java.io.BufferedWriter;
+
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
 
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
@@ -15,6 +25,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 
 import interfaces.IFileReader;
+import model.Researcher;
+import model.ResearcherCollection;
 
 public class XmlFileIO implements IFileReader {
 
@@ -95,4 +107,128 @@ public class XmlFileIO implements IFileReader {
 		// TODO Auto-generated method stub
 		return null;
 	}
+	public void updateXmlWithNewResearcher(String path, Researcher newResearcher) {
+	    try {
+	        File inputFile = new File(path);
+	        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+	        DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+	        Document doc = dBuilder.parse(inputFile);
+	        doc.getDocumentElement().normalize();
+	        
+	        removeExistingResearcher(doc, newResearcher.getUsername());
+	        
+	        
+	        Element researcherElement = createResearcherElement(doc, newResearcher);
+	        Element followingResearchersElement = createFollowingResearchersElement(doc, newResearcher);
+	        Element followerResearchersElement = createFollowerResearchersElement(doc, newResearcher);
+
+	        researcherElement.appendChild(followingResearchersElement);
+	        researcherElement.appendChild(followerResearchersElement);
+	        
+	        Element researchersElement = doc.getDocumentElement();
+	        researchersElement.appendChild(researcherElement);
+
+	        saveDocumentToFile(doc, inputFile);
+
+	        System.out.println("XML file updated successfully.");
+	    } catch (Exception e) {
+	        e.printStackTrace();
+	    }
+	}
+
+	private Element createResearcherElement(Document doc, Researcher newResearcher) {
+	    Element researcherElement = doc.createElement("researcher");
+
+	    Element usernameElement = doc.createElement("researcher_name");
+	    usernameElement.appendChild(doc.createTextNode(newResearcher.getUsername()));
+	    researcherElement.appendChild(usernameElement);
+
+	    Element passwordElement = doc.createElement("password");
+	    passwordElement.appendChild(doc.createTextNode(newResearcher.getPassword()));
+	    researcherElement.appendChild(passwordElement);
+
+	    return researcherElement;
+	}
+
+	private Element createFollowingResearchersElement(Document doc, Researcher newResearcher) {
+	    Element followingResearchersElement = doc.createElement("following_researcher_names");
+	    for (Object followingResearcher : newResearcher.getFollowingResearchers()) {
+	        Element followingResearcherNameElement = doc.createElement("following_researcher_name");
+	        followingResearcherNameElement.appendChild(doc.createTextNode(followingResearcher.toString()));
+	        followingResearchersElement.appendChild(followingResearcherNameElement);
+	    }
+	    return followingResearchersElement;
+	}
+
+	private Element createFollowerResearchersElement(Document doc, Researcher newResearcher) {
+	    Element followerResearchersElement = doc.createElement("follower_researcher_names");
+	    for (Object followerResearcher : newResearcher.getFollowerResearchers()) {
+	        Element followerResearcherNameElement = doc.createElement("follower_researcher_name");
+	        followerResearcherNameElement.appendChild(doc.createTextNode(followerResearcher.toString()));
+	        followerResearchersElement.appendChild(followerResearcherNameElement);
+	    }
+	    return followerResearchersElement;
+	}
+
+	private void saveDocumentToFile(Document doc, File inputFile) throws Exception {
+	    TransformerFactory transformerFactory = TransformerFactory.newInstance();
+	    Transformer transformer = transformerFactory.newTransformer();
+	    transformer.setOutputProperty(OutputKeys.INDENT, "yes"); // Set indent property to "yes"
+	    transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "2"); // Set indent amount to 2 or your desired value
+	    transformer.setOutputProperty(OutputKeys.OMIT_XML_DECLARATION, "yes"); // Omit XML declaration
+	    transformer.setOutputProperty("{http://xml.apache.org/xalan}indent-amount", "0"); // Disable indentation for existing content
+
+	    // Serialize the document to a string
+	    StringWriter writer = new StringWriter();
+	    StreamResult result = new StreamResult(writer);
+	    transformer.transform(new DOMSource(doc), result);
+	    String xmlString = writer.toString();
+
+	    // Remove indentation for existing content
+	    xmlString = xmlString.replaceAll("\n\\s+", "\n");
+
+	    // Write the modified XML string to the file
+	    try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(inputFile))) {
+	        bufferedWriter.write(xmlString);
+	    }
+	}
+
+
+	
+	private void removeExistingResearcher(Document doc, String username) {
+        NodeList researcherNodes = doc.getElementsByTagName("researcher");
+
+        for (int i = 0; i < researcherNodes.getLength(); i++) {
+            Node researcherNode = researcherNodes.item(i);
+            if (researcherNode.getNodeType() == Node.ELEMENT_NODE) {
+                Element researcherElement = (Element) researcherNode;
+                String existingUsername = researcherElement.getElementsByTagName("researcher_name").item(0).getTextContent();
+                System.out.println("exist "+ existingUsername +" given: "+ username);
+                if (existingUsername.equals(username)) {
+                	System.out.println("girdim");
+                    researcherNode.getParentNode().removeChild(researcherNode);
+                    break;
+                }
+            }
+        }
+    }
+	 public static void main(String[] args) {
+	        // Path to the XML file
+	        String xmlFilePath = "OpenResearch-MVC/src/users.xml";
+
+	        // Create a new researcher
+	        Researcher newResearcher = new Researcher();
+	        newResearcher.setUsername("Berke Tınas");
+	        newResearcher.setPassword("123");
+	        newResearcher.setFollowingResearchers(Arrays.asList(""));
+	        newResearcher.setFollowerResearchers(Arrays.asList("Göktay İncekara","抑菌"));
+
+	        // Update the XML file with the new researcher
+	        XmlFileIO xmlFileIO = new XmlFileIO();
+	        xmlFileIO.updateXmlWithNewResearcher(xmlFilePath, newResearcher);
+	        ResearcherCollection rc = new ResearcherCollection();
+	        rc.createCollection();
+	    }
+	 
+	
 	}

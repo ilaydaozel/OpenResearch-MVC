c0b73e1d551ca96199fc267e6ba99e4bff92101c
ilaydaozel <ilaydaoozel@gmail.com>
2023-05-20 17:15:55 +0300

    Add follow lists

--- src/view/AccountPage.java
+++ src/view/AccountPage.java
@@ -16,8 +16,8 @@ public class AccountPage extends JPanel implements java.util.Observer{
     private JLabel username;
 	private JLabel followerLabel = new JLabel("Follower Researchers:");
 	private JLabel followingLabel = new JLabel("Following Researchers:");
-    private ListContainer followingList;
-    private ListContainer followerList;
+    private JList followingList;
+    private JList followerList;
     private Color blue = new Color(144, 219, 244);
     
     public AccountPage( Researcher researcher) {
@@ -28,12 +28,26 @@ public class AccountPage extends JPanel implements java.util.Observer{
 
     private void initComponents() {
     	username = new JLabel(researcher.getUsername(), SwingConstants.RIGHT);
-		followerList = new ListContainer(researcher.getFollowerResearchers(), 250, 70);
-		followingList = new ListContainer(researcher.getFollowingResearchers(), 250, 70);
-    	
+ 
     	//panel design
         setLayout(new GridBagLayout());
         GridBagConstraints gridBagConstraints = new GridBagConstraints();
+        
+        //follower list
+        followerList = new JList(researcher.getFollowerResearchers().toArray());
+        followerList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        followerList.setLayoutOrientation(JList.VERTICAL);
+        followerList.setVisibleRowCount(-1);
+        JScrollPane followerListScroller = new JScrollPane(followerList);  
+        followerListScroller.setPreferredSize(new Dimension(200,70));
+        
+        //following list
+        followingList = new JList(researcher.getFollowingResearchers().toArray());
+        followingList.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        followingList.setLayoutOrientation(JList.VERTICAL);
+        followingList.setVisibleRowCount(-1);
+        JScrollPane followingListScroller = new JScrollPane(followingList);  
+        followingListScroller.setPreferredSize(new Dimension(200,70));
         
         //label
         usernameLabel.setFont(new Font("", Font.BOLD, 18));
@@ -70,27 +84,27 @@ public class AccountPage extends JPanel implements java.util.Observer{
         followerPanel.setLayout(new BoxLayout(followerPanel, BoxLayout.Y_AXIS));
         followerLabel.setFont(new Font("", getFont().getStyle(),16));
         followerLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        followerList.setAlignmentX(Component.LEFT_ALIGNMENT);
+        followerListScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
         followerPanel.add(followerLabel);
-        followerPanel.add(followerList);
+        followerPanel.add(followerListScroller);
         
         //following panel
         JPanel followingPanel = new JPanel();
         followingPanel.setLayout(new BoxLayout(followingPanel, BoxLayout.Y_AXIS));
         followingLabel.setFont(new Font("", getFont().getStyle(),16));
         followingLabel.setAlignmentX(Component.LEFT_ALIGNMENT);
-        followingList.setAlignmentX(Component.LEFT_ALIGNMENT);
+        followingListScroller.setAlignmentX(Component.LEFT_ALIGNMENT);
         followingPanel.add(followingLabel);
-        followingPanel.add(followingList);
+        followingPanel.add(followingListScroller);
         
         followGridBagConstraints.gridx = 0;
         followGridBagConstraints.gridy = 0;
-        followGridBagConstraints.insets = new Insets(15, 5, 15, 5);
+        followGridBagConstraints.insets = new Insets(15, 10, 15, 5);
         followPanel.add(followerPanel, followGridBagConstraints );
         
         followGridBagConstraints.gridx = 1;
         followGridBagConstraints.gridy = 0;
-        followGridBagConstraints.insets = new Insets(15, 5, 15, 5);
+        followGridBagConstraints.insets = new Insets(15, 10, 15, 5);
         followPanel.add(followingPanel, followGridBagConstraints );
         
         Border followPanelBorder = BorderFactory.createLineBorder(Color.BLACK, 1);
@@ -102,7 +116,6 @@ public class AccountPage extends JPanel implements java.util.Observer{
         gridBagConstraints.gridy = 1;
         gridBagConstraints.anchor = GridBagConstraints.WEST;
         add(followPanel, gridBagConstraints);
-
         
     }
 
@@ -111,7 +124,8 @@ public class AccountPage extends JPanel implements java.util.Observer{
 		// TODO Auto-generated method stub
 		System.out.println("********inside update method accountpage");
 		System.out.println(researcher.getFollowingResearchers());
-		System.out.println("--------------updated Accountpage----------");
+		followerList.setListData(researcher.getFollowerResearchers().toArray());
+		followingList.setListData(researcher.getFollowingResearchers().toArray());
 		
 	}
    

04e79e185b96618b5f8f2a9223183c7cd8b5c79d
Mert Türker <mertturker99@gmail.com>
2023-05-21 11:29:14 +0300

    Update Collection.java

--- src/model/Collection.java
+++ src/model/Collection.java
@@ -3,13 +3,17 @@ package model;
 import java.util.List;
 import java.util.Map;
 import java.util.Observable;
+
+import fileIO.CsvFileIO;
+
 import java.util.ArrayList;
 import interfaces.IFileReader;
+import interfaces.IFileWriter;
 
 public abstract class Collection extends Observable {
     private List<Object> collection = new ArrayList<>();
     private IFileReader reader;
-
+    private IFileWriter writer;
     public Collection() {
         
         
@@ -27,7 +31,7 @@ public abstract class Collection extends Observable {
         this.collection = collection;
     }
 
-
+    
     public void addToCollection(Object item) {
     	collection.add(item);
     }
@@ -43,4 +47,16 @@ public abstract class Collection extends Observable {
 	public void setReader(IFileReader reader) {
 		this.reader = reader;
 	}
+
+	public IFileWriter getWriter() {
+		return writer;
+	}
+
+	public void setWriter(IFileWriter writer) {
+		this.writer = writer;
+	}
+    public void updateFile() {
+        this.writer.writeAllPapers(this.collection);
+    }
+   
 }

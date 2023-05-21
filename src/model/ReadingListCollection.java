f8612e7f64d0e2d16e3f5c470fae22d4512e0562
Mert Türker <mertturker99@gmail.com>
2023-05-21 12:36:55 +0300

    Update ReadingListCollection.java

--- src/model/ReadingListCollection.java
+++ src/model/ReadingListCollection.java
@@ -1,5 +1,8 @@
 package model;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Map;
 
 import fileIO.JSONFileIO;
@@ -14,14 +17,34 @@ public class ReadingListCollection extends Collection{
 	}
 	@Override
 	public void createCollection() {
-		
+		List<Map<String, String>> allReadingLists;
+		allReadingLists = getReader().readAllElements("readingList.json");
+
+    	for (Map<String, String> data : allReadingLists) {
+    		addToCollection(createCollectionElement(data));
+        }  
 		
 	}
 
 	@Override
 	public Object createCollectionElement(Map<String, String> data) {
-		// TODO Auto-generated method stub
-		return null;
+		
+		ResearcherCollection researcherCollection = new ResearcherCollection();
+		Researcher researcher =researcherCollection.getResearcherByResearcherName(data.get("creator_researcher_name"));
+		
+		ReadingList readingList = new ReadingList(
+											Integer.parseInt(data.get("readinglist_id")),
+											Integer.parseInt(data.get("number_of_papers")),
+											data.get("name_of_papers").split(","),
+											data.get("creator_researcher_name"),
+											researcher);
+				
+		System.out.println("readinglist_id: "+ data.get("readinglist_id"));
+		System.out.println("number_of_papers: "+ data.get("number_of_papers"));
+		System.out.println("name_of_papers: "+ data.get("name_of_papers"));
+		System.out.println("test follwing "+ readingList.getCreatorResearcher().getUsername());
+		return readingList;		
+		
 	}
 
 }
